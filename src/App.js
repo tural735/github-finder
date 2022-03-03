ac11d8bf43f87e4672dfc4c75d7b7b100ebfb228
Tural Memmedzade <49388337+tural735@users.noreply.github.com>
2022-03-04 01:49:14 +0400

    .

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar/>
+        <main>
+          Content
+        </main>
+      </div>
+    </Router>
+      
+  );
+}
+
+export default App;
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,31 @@
+import {FaGithub} from 'react-icons/fa';
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+function Navbar({title}){
+    return(
+        <nav className='navbar mb-2 shadow-lg bg-neutral text-neutral text-neutral-content'>
+            <div className='container mx-auto'>
+                <div className='flex-none px-2 mx-2'>
+                    <FaGithub className='inline pr-2 text-3xl'/>
+                    <Link to="/" className='text-lg font-bold align-middle'>{title}</Link>
+                </div>
+                <div className='flex-1 px-2 mx-2'>
+                    <div className="flex justify-end">
+                        <Link to="/" className='btn btn-ghost btn-sm rounded bg-opacity-10'>Home</Link>
+                        <Link to="/about" className='btn btn-ghost btn-sm rounded'>About</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.defaultProps={
+    title: "Github Finder"
+}
+
+Navbar.propTypes={
+    title: PropTypes.string
+}
+
+export default Navbar;
