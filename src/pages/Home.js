466b476ea19d41ff22193b439094bc7903627233
Tural Memmedzade <49388337+tural735@users.noreply.github.com>
2022-03-09 01:13:45 +0400

    User result and Loading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import UserResult from '../components/users/UserResult'
 
 function Home() {
   return (
     <div>
-        <h1 className='text-6xl'>Welcome</h1>
+      <UserResult/>
     </div>
   )
 }
