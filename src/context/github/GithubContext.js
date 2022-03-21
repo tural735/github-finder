bb062c6826daea25fab12b2ebecab3bd5758cb37
Tural Memmedzade <49388337+tural735@users.noreply.github.com>
2022-03-22 02:10:38 +0400

    user profil top

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,7 @@ const GITHUB_TOKEN=process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider=({children})=>{
     const initialState={
         users:[],
+        user:{},
         loading: false
     }
     const [state,dispatch]=useReducer(githubReducer,initialState)
@@ -29,6 +30,28 @@ export const GithubProvider=({children})=>{
             payload: items
         })
     }
+    // get single user
+    const getUser = async (login) => {
+        setLoading()
+ 
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+ 
+        if(response.status === 404) {
+            window.location ='/notfound'
+        } else {
+            const data = await response.json()
+ 
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+    }
+
 
     const clearUsers=()=>dispatch({type:"CLEAR_USERS"})
     const setLoading=()=>dispatch({type:"SET_LOADING"})
@@ -36,8 +59,10 @@ export const GithubProvider=({children})=>{
     return <GithubContext.Provider value={{
         users: state.users,
         loading:state.loading,
+        user: state.user,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser,
         }}>
         {children}
     </GithubContext.Provider>
